8d2a6032623d39470d71ed15f49d4d6449f6c0bc
Tim Green <timothy.green@gmail.com>
2014-12-24 16:08:22 +0000

    make constituency matcher a bit more robust

--- scripts/match_constituencies.py
+++ scripts/match_constituencies.py
@@ -3,55 +3,77 @@ import json
 from pymongo import MongoClient
 from collections import Counter
 
-constituencies = json.load(open('parse_data/constituencies.json'))
-
 client = MongoClient()
 
 db = client.news.articles
 
 docs = db.find()
 
+cardinal_directions = ['north east', 'south east', 'south west', 'north west', 'north', 'south', 'east', 'west']
+
 def score_match(constituency, text, title):
-  names = [constituency]
-  name_tokens = constituency.split()
+    names = [constituency]
+    name_tokens = constituency.split()
 
-  #if len(name_tokens) > 2:
-  #  names.append(" ".join(name_tokens[1:]))
+    # Replace " and " with " & "
+    for name in list(names):
+        if " and " in name:
+            names.append(name.replace(" and ", " & "))
 
-  #match_party = False
+    # Transpose directions, e.g. Durham North -> North Durham
+    for name in list(names):
+        for direction in cardinal_directions:
+          if constituency.endswith(direction):
+            name_ = direction + " " + constituency[:-len(direction)].strip()
+            names.append(name_)
+            break
 
-  #for party in candidate['parties']:
-  #  if party['name'] in text or party['name'] in title:
-  #    match_party = True
+    # Undo commas, e.g. Durham, City of -> City of Durham
+    # Also remove commas e.g. Sheffield, Hallam -> Sheffield Hallam
+    # And remove first word, e.g. Shieffield, Hallam -> Hallam
+    for name in list(names):
+        if ',' in constituency:
+            tokens = [x.strip() for x in constituency.split(',')]
 
-  for name in names:
-    if name in text or name in title:
-      return 1.0
+            names.append(" ".join(tokens))
+
+            if " and " not in constituency:
+                names.append(" ".join(tokens[1:]))
+                names.append(" ".join(reversed(tokens)))
+
+    names = [x for x in names if x != 'test']
+
+    for name in names:
+        if name in text or name in title:
+            return 1.0
     
-  return 0.0
-  
-for doc in docs:
-  if doc['page'] is None:
-    continue
-  
-  print doc['key']
+    return 0.0
 
-  text = doc['page']['text']
-  title = doc['page']['title']
+if __name__ == "__main__": 
+    constituencies = json.load(open('parse_data/constituencies.json'))
 
-  possible_constituency_matches = {}
+    for doc in docs:
+      if doc['page'] is None:
+        continue
+      
+      print doc['key']
 
-  for constituency in constituencies:
-    score = score_match(constituency['name'], text, title)
+      text = doc['page']['text'].lower()
+      title = doc['page']['title'].lower()
 
-    if score > 0.0:
-      print constituency['name'], score
+      possible_constituency_matches = {}
 
-      constituency['score'] = score
-      constituency['id_snip'] = constituency['id'].split(':')[1]
-      possible_constituency_matches[constituency['id_snip']] = constituency
+      for constituency in constituencies:
+        score = score_match(constituency['name'].lower(), text, title)
 
-  doc['possible_constituency_matches'] = possible_constituency_matches.values()
+        if score > 0.0:
+          print constituency['name'], score
 
-  db.save(doc)
+          constituency['score'] = score
+          constituency['id_snip'] = constituency['id'].split(':')[1]
+          possible_constituency_matches[constituency['id_snip']] = constituency
+
+      doc['possible_constituency_matches'] = possible_constituency_matches.values()
+
+      db.save(doc)
 
