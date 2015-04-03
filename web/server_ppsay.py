2f9f5e604a834c766cc1b0cfd759dcb1e008b157
Tim Green <timothy.green@gmail.com>
2015-04-03 10:43:58 +0100

    add export.json endpoint

--- web/server_ppsay.py
+++ web/server_ppsay.py
@@ -258,6 +258,42 @@ def constituency(constituency_id, rss=False):
                                candidates=candidate_docs,
                                area=area_doc)
 
+@app.route('/export.json', methods=['GET'])
+def export():
+    export_data = {}
+
+    for constituency_id in constituencies_index:
+        candidate_docs = db_candidates.find({'deleted': {'$ne': True},
+                                           '$or': [{"candidacies.2010.constituency.id": str(constituency_id)},
+                                                   {"candidacies.2015.constituency.id": str(constituency_id)}]})
+
+        candidate_docs = sorted(candidate_docs, key=lambda x: x['name'])
+
+        candidate_ids = [x['id'] for x in candidate_docs if ('2015' in x['candidacies'] and x['candidacies']['2015']['constituency']['id'] == str(constituency_id)) \
+                                                           or x['incumbent']]
+
+        article_docs = db_articles.find({'state': 'approved',
+                                       '$or': [{'constituencies': {'$elemMatch': {'id': str(constituency_id), 'state': {'$ne': 'removed'}}}},
+                                               {'candidates': {'$elemMatch': {'id': {'$in': candidate_ids}, 'state': {'$ne': 'removed'}}}}]}) \
+                                 .sort([["time_added", -1]])
+
+        article_docs = list(article_docs)
+        for article_doc in article_docs:
+            if article_doc['page']['date_published']:
+                article_doc['order_date'] = article_doc['page']['date_published']
+            else:
+                article_doc['order_date'] = article_doc['time_added']
+        
+        
+        article_docs = sorted(article_docs, key=lambda x: x['order_date'], reverse=True)
+
+        export_data[constituency_id] = [{'url': doc['page']['urls'][0],
+                                         'title': doc['page']['title'],
+                                         'source': doc['domain'],
+                                         'date': doc['order_date'].isoformat(),} for doc in article_docs[:10]]
+
+    return jsonify(export_data)
+
 @app.route('/article', methods=['POST'])
 def article_add():
     url = request.form.get('url', None)
