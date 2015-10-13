f721cafaa71d6a6be74a33710f279e384c921240
Tim Green <timothy.green@gmail.com>
2015-10-13 08:52:45 +0100

    update page migration script to handle articles based on multiple pages

--- scripts/migrate_pages.py
+++ scripts/migrate_pages.py
@@ -1,22 +1,42 @@
 from ppsay.page import Page
 from ppsay.db import db_articles
+from urlparse import urlparse
 
 for doc in db_articles.find():
-    d = doc['page']
+    if 'page' not in doc:
+        print "Skipping"
+        continue
 
-    d['url'] = d['urls'][0]
-    d['final_url'] = d['final_urls'][0]
-    d['source'] = doc['source']
-    d['domain'] = doc['domain']
+    page_ids = []
 
-    page = Page(d)
+    for url, final_url in zip(doc['page']['urls'], doc['page']['final_urls']):
+        print url, final_url
 
-    print page.save()
+        d = doc['page']
 
-    doc['pages'] = [page._id]
+        d['url'] = url
+        d['final_url'] = final_url
+        d['source'] = doc['source']
+        d['domain'] = urlparse(final_url).netloc
+
+        page = Page.get_url(d['url'])
+
+        if page is None:
+            print "Creating {}".format(d['url'])
+            page = Page(d)
+            page.save()
+        else:
+            print "Exists"
+
+        page_ids.append(page._id)
+
+    doc['pages'] = page_ids
+
     del doc['page']
     del doc['source']
     del doc['domain']
+    
+    print doc
 
-    db_articles.save(doc)
+    #db_articles.save(doc)
 
